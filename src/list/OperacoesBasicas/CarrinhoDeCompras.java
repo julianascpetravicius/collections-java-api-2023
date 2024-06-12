bf4a4338ad7f02367df9bfc94b7eb02785e04f70
Juliana Petravicius <135447143+julianascpetravicius@users.noreply.github.com>
2024-06-12 18:26:32 +0100

    create CarrinhoDeCompras

---
+++ src/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,63 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    private List<Item> carrinhoDeCompras;
+
+    public CarrinhoDeCompras() {
+        this.carrinhoDeCompras = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        Item item = new Item(nome, preco, quantidade);
+        this.carrinhoDeCompras.add(item);
+    }
+
+    public void removerItem(String nome){
+        List<Item> itensParaRemover = new ArrayList<>();
+        for(Item i: carrinhoDeCompras) {
+            if(i.getNome().equalsIgnoreCase(nome)) {
+                itensParaRemover.add(i);
+            }
+        }
+        carrinhoDeCompras.removeAll(itensParaRemover);
+    }
+
+    public double obterValorTotal(){
+        double valorTotal = 0;
+        for(Item i: carrinhoDeCompras) {
+            valorTotal += i.getPreco() * i.getQuantidade();
+        }
+        return valorTotal;
+    }
+
+    public void exibirItens(){
+        if (!carrinhoDeCompras.isEmpty()) {
+            System.out.println(this.carrinhoDeCompras);
+        } else {
+            System.out.println("O carrinho está vazio");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CarrinhoDeCompras{" +
+                "itens=" + this.carrinhoDeCompras +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+        carrinhoDeCompras.adicionarItem("Bolacha", 2d, 2);
+        carrinhoDeCompras.exibirItens();
+        carrinhoDeCompras.adicionarItem("Bolacha", 2d, 2);
+        carrinhoDeCompras.exibirItens();
+        carrinhoDeCompras.adicionarItem("Sabão", 2d, 2);
+        carrinhoDeCompras.exibirItens();
+        carrinhoDeCompras.removerItem("Bolacha");
+        carrinhoDeCompras.exibirItens();
+    }
+}
